9e9de59d4cd052e55d0bca98c2043b0edfb0164a
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-05-01 14:54:32 -0400

    commit tree 3a670237bc6245a5e8615522e9200e9db41807a7

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  let result;
+  if( x<=400){
+    result = "This one is on me!";
+  }
+  else if (x<=2000){
+    result = "That will be twenty bucks."
+  }
+  else if (x<=2500){
+    result = "I will gladly take your thirty bucks."
+  }
+  else if (x>2500){
+    result = "No can do."
+  }
+
+return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  if(city==='NYC'){
+    result="Ok, sounds good."
+  }
+  else if(city!== 'NYC'){
+    result="No go."
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  let response;
+  switch(tips){
+    case 'generous':
+      return response='Thank you so much.';
+      break;
+    case 'not as generous':
+      return response="Thank you.";
+      break;
+   default:
+     return response="Bye.";
+
+
+  }
 }
